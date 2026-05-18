a3a4f14c80c5a691f87ae870d65d46467b27cd3b
mamadou-adiouma <adiou.xyz@gmail.com>
2026-05-18 21:19:10 +0000

    Modification apportée - annulation

--- src/main/java/sn/uvs/studentapi/controller/StudentController.java
+++ src/main/java/sn/uvs/studentapi/controller/StudentController.java
@@ -1,15 +1,7 @@
 package sn.uvs.studentapi.controller;
 
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import sn.uvs.studentapi.model.Student;
 import sn.uvs.studentapi.service.StudentService;
 
@@ -18,9 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-/**
- * Service utilisé pour les opérations métier sur les étudiants.
- */
+
     private final StudentService service;
 
     public StudentController(StudentService service) {
--- src/main/java/sn/uvs/studentapi/model/package-info.java
+++
@@ -1,1 +0,0 @@
-package sn.uvs.studentapi.model;
