432b7bfcee4def1d3017e79b234218f7eada126d
mamadou-adiouma <adiou.xyz@gmail.com>
2026-05-18 20:41:51 +0000

    Modification apportée sur les imports et commentaires javadocs

--- src/main/java/sn/uvs/studentapi/controller/StudentController.java
+++ src/main/java/sn/uvs/studentapi/controller/StudentController.java
@@ -1,7 +1,15 @@
 package sn.uvs.studentapi.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import sn.uvs.studentapi.model.Student;
 import sn.uvs.studentapi.service.StudentService;
 
@@ -10,7 +18,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-
+/**
+ * Service utilisé pour les opérations métier sur les étudiants.
+ */
     private final StudentService service;
 
     public StudentController(StudentService service) {
